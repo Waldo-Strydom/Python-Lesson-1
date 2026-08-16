8a711ab338f3cb9f5b266091d92f6c24d144ba80
Waldo Strydom <88497072+Waldo-Strydom@users.noreply.github.com>
2026-08-16 18:09:04 +0200

    Add files via upload

---
+++ Lesson 1/Lesson 1 code.py
@@ -0,0 +1,29 @@
+# print("Hello")
+
+# countDown = 10
+# while countDown>=0:
+#     print(countDown)
+#     countDown-=1
+# print("blastoff")
+
+# password = "abc"
+# userInput = ""
+# while userInput!= password:
+#     userInput = input("Please type in password ")
+
+# money = 0
+# while money <100:
+#     print(money)
+#     money+=int(input("Please insert your money "))
+
+# keep_counting = True
+# count = 0
+# while keep_counting == True:
+#     count+=1
+#     print(count, keep_counting)
+#     if count==5:
+#         keep_counting = False
+# print(keep_counting)
+
+
+
